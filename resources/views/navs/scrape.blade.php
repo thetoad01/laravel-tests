2de269166a8c92de852b8ff6b22be84b9d33b3bf
David D <david@defoenet.com>
2020-06-09 14:50:32 -0400

    Minor ui fixes

---
+++ resources/views/navs/scrape.blade.php
@@ -0,0 +1,13 @@
+<div class="row stylish-color text-white">
+    <ul class="nav px-4">
+        <li class="nav-link">
+            <a href="{{ route('scrape.cdk-vdp.index') }}" class="nav-link text-white">Un-Processed VDPs</a>
+        </li>
+        <li class="nav-link">
+            <a href="{{ route('scrape.cdk-vdp.process') }}" class="nav-link text-white">Process All VDPs</a>
+        </li>
+        <li class="nav-link">
+            <a href="{{ route('scrape.cdk-sitemap.index') }}" class="nav-link text-white">Sitemaps</a>
+        </li>
+    </ul>
+</div>
