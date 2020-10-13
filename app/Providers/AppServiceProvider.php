d74aac883f1ac8c43e752f14ee52f59ec4252d30
David <david@defoenet.com>
2020-10-13 15:40:33 -0400

    Upgraded to Laravel 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\NhtsaDecoded;
 use App\Observers\NhtsaDecodeObserver;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         NhtsaDecoded::observe(NhtsaDecodeObserver::class);
+        Paginator::useBootstrap();
     }
 }
