699908a01eb8533dbbea409e60b10a2f09eaba6a
David <david@defoenet.com>
2020-12-05 15:00:08 -0500

    Error fix

--- app/Http/Controllers/Scrape/CdkSitemapController.php
+++ app/Http/Controllers/Scrape/CdkSitemapController.php
@@ -132,12 +132,6 @@ class CdkSitemapController extends Controller
      */
     public function scrape($id)
     {
-        $data = (new \App\Clients\CdkSitemapClient)->handle('https://www.ujchevy.com/sitemap-inventory-sincro.xml');
-
-        if (!$data['data']) {
-            dd('no data');
-        }
-
         $sitemap = CdkSitemap::find($id);
 
         $data = (new \App\Clients\CdkSitemapClient)->handle($sitemap->sitemap_url);
