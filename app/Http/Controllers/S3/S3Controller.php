4a95492aef719bddf9dbac3ad24b69571545cbc1
David <david@defoenet.com>
2021-02-20 15:03:41 -0500

    Test storage return response

--- app/Http/Controllers/S3/S3Controller.php
+++ app/Http/Controllers/S3/S3Controller.php
@@ -22,4 +22,19 @@ class S3Controller extends Controller
     {
         return view('s3.create');
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'photo' => 'required|image|max:3000',
+        ]);
+
+        $result_local = Storage::disk('public')->putFileAs('a4f5', $request->photo, 'upload.jpg', 'public');
+        $result_s3 = Storage::disk('s3')->putFileAs('a4f5', $request->photo, 'upload.jpg', 'public');
+
+        return [
+            'local' => Storage::disk('public')->url($result_local),
+            'S3' => Storage::disk('s3')->url($result_s3),
+        ];
+    }
 }
--- resources/views/s3/create.blade.php
+++ resources/views/s3/create.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Laravel Storage Tests | Update An Image')
 
 @section('heads')
+@livewireStyles
 @endsection
 
 @section('content')
@@ -28,31 +29,35 @@
 <div class="container mx-auto mb-6 p-4 bg-gray-200">
     <h1 class="text-2xl font-bolder text-indigo-800">Upload an Image</h1>
 
-    <div>
-        <label class="block text-sm font-medium text-gray-700">
-            photo
-        </label>
-
-        <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md bg-white">
-            <div class="space-y-1 text-center">
-                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-                <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                        <span>Upload a file</span>
-                        <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
+    <div class="my-4 flex items-center">
+        {{-- <livewire:photo-upload :photo="$photo ?? ''" /> --}}
+        <form action="{{ route('s3.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+    
+            <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:mt-px sm:pt-2">
+                <label for="photo" class="block text-sm leading-5 font-medium text-gray-700 sm:mt-px sm:pt-2">Upload Photo</label>
+                <div class="mt-2 sm:mt-0 sm:col-span-2">
+                    <input type="file" name="photo">
+                    @error('photo')
+                        <p class="text-red-500 mt-1">{{ $message }}</p>
+                    @enderror
+    
+                    <div class="mt-4">
+                        @isset($photo->name)
+                            <img src="{{ Storage::url($photo->name) }}" alt="uploaded photo">
+                        @endisset
+                    </div>
                 </div>
-                <p class="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 3MB
-                </p>
             </div>
-        </div>
+
+            <div class="my-4">
+                <button type="submit" class="px-4 py-2 text-white bg-blue-800">Upload</button>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
 
 @section('scripts')
+@livewireScripts
 @endsection
